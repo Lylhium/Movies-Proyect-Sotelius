c0e50b890c0c0ad7a5ebfc5742ec4318332b4ddb
Agustin Pfarherr <85139943+Lylhium@users.noreply.github.com>
2023-09-26 16:55:10 -0300

    movies landing implementation

--- sotelius-movies/src/app/movies/page.tsx
+++ sotelius-movies/src/app/movies/page.tsx
@@ -1,13 +1,10 @@
 "use client";
-
-import CardUI from "@/components/card";
 import Image from "next/image";
 import React from "react";
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 '>
       <h1> movie landing </h1>
-      <CardUI />
     </main>
   );
 }
