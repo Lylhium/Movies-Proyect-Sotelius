20bc0d547f46e3ac3cbb2766fa3bb29f1a47841a
Agustin Pfarherr <85139943+Lylhium@users.noreply.github.com>
2023-10-03 16:50:04 -0300

    Footer and MovieDetails Implementation

--- sotelius-movies/src/components/card.tsx
+++ sotelius-movies/src/components/card.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-
+import Link from "next/link";
 export default function App({ movie }: any) {
   const IMG_BASE_URL = "https://www.themoviedb.org/t/p/w220_and_h330_face";
 
   return (
     <div className='card w-96 bg-base-100 shadow-xl'>
-      <figure>
-        <img src={IMG_BASE_URL + movie.poster_path} alt='movie' />
-      </figure>
-      <div className='card-body'>
-        <h2 className='card-title'>{movie.title}</h2>
-        <p>{movie.overview}</p>
-        <div className='card-actions justify-end'>
-          <button className='btn btn-primary'>see more info.</button>
+      <Link href={"/movie/" + movie.id}>
+        <figure>
+          <img src={IMG_BASE_URL + movie.poster_path} alt='movie' />
+        </figure>
+        <div className='card-body'>
+          <h2 className='card-title'>{movie.title}</h2>
+          <p>{movie.overview}</p>
+          <div className='card-actions justify-end'>
+            <button className='btn btn-primary'>see more info.</button>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
