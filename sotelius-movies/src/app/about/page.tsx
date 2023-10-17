290ae9ff4c01c348a9f18c3ed1387b5bc7accfc1
Agustin Pfarherr <85139943+Lylhium@users.noreply.github.com>
2023-10-17 20:17:24 -0300

    comm

--- sotelius-movies/src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import React from "react";
-export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 '>
-      <h1> about us landing</h1>
-    </main>
-  );
-}
